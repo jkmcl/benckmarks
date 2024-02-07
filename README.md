b66f98fef0872ea722f12724d88172a476c2cb38
Michael Lee <jkmcl@users.noreply.github.com>
2024-02-08 00:47:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A sample Java Microbenchmark Harness (JMH) project using Spring Boot for dependency and plugin management.
+
+The POM and Java class are based on those from the JMH Maven archetype:
+* [pom.xml](https://github.com/openjdk/jmh/blob/1.37/jmh-archetypes/jmh-java-benchmark-archetype/src/main/resources/archetype-resources/pom.xml)
+* [MyBenchmark.java](https://github.com/openjdk/jmh/blob/1.37/jmh-archetypes/jmh-java-benchmark-archetype/src/main/resources/archetype-resources/src/main/java/MyBenchmark.java)
---
+++ src/main/java/my/Benchmarks.java
@@ -0,0 +1,34 @@
+package my;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+
+@Fork(1)
+public class Benchmarks {
+
+	@Benchmark
+	public String formatDateWithSimpleDateFormat() {
+		return (new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")).format(new Date());
+	}
+
+	// DateTimeFormatter is thread-safe
+	private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS")
+			.withZone(ZoneId.systemDefault());
+
+	@Benchmark
+	public String formateInstantWithDateTimeFormatter() {
+		return dtf.format(Instant.now());
+	}
+
+	@Benchmark
+	public String formateDateWithDateTimeFormatter() {
+		return dtf.format((new Date()).toInstant());
+	}
+
+}
